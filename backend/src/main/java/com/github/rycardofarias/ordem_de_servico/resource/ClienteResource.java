ed9f397f03cdd7fbca138eedceff08235600efec
Ricardo Farias <ricardofariasxyz@gmail.com>
2021-07-21 14:33:31 -0300

    Get clinte findAll

--- backend/src/main/java/com/github/rycardofarias/ordem_de_servico/resource/ClienteResource.java
+++ backend/src/main/java/com/github/rycardofarias/ordem_de_servico/resource/ClienteResource.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @RestController
 @RequestMapping(value="/clientes")
@@ -21,5 +24,11 @@ public class ClienteResource {
     public ResponseEntity<ClienteDTO> findById(@PathVariable Integer id){
         ClienteDTO objDTO = new ClienteDTO(clienteService.findById(id));
         return ResponseEntity.ok().body(objDTO);
+    }
+    @GetMapping
+    public ResponseEntity<List<ClienteDTO>> findAll(){
+        List<ClienteDTO> listDTO = clienteService.findAll()
+                .stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
