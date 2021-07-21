e9fd2495f45c162095c8c6b3c8b68a087bb9b652
Ricardo Farias <ricardofariasxyz@gmail.com>
2021-07-21 15:39:51 -0300

    Put cliente

--- backend/src/main/java/com/github/rycardofarias/ordem_de_servico/resource/ClienteResource.java
+++ backend/src/main/java/com/github/rycardofarias/ordem_de_servico/resource/ClienteResource.java
@@ -1,14 +1,15 @@
 package com.github.rycardofarias.ordem_de_servico.resource;
 
+import com.github.rycardofarias.ordem_de_servico.domain.Cliente;
 import com.github.rycardofarias.ordem_de_servico.dto.ClienteDTO;
 import com.github.rycardofarias.ordem_de_servico.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,5 +31,19 @@ public class ClienteResource {
         List<ClienteDTO> listDTO = clienteService.findAll()
                 .stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
+    }
+    @PostMapping
+    public ResponseEntity<ClienteDTO> create(@Valid @RequestBody ClienteDTO clienteDTO){
+        Cliente newObj = clienteService.create(clienteDTO);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newObj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClienteDTO> update (@PathVariable Integer id, @Valid @RequestBody ClienteDTO clienteDTO){
+        ClienteDTO newObj = new ClienteDTO(clienteService.update(id, clienteDTO));
+        return ResponseEntity.ok().body(newObj);
     }
 }
--- backend/src/main/java/com/github/rycardofarias/ordem_de_servico/service/ClienteService.java
+++ backend/src/main/java/com/github/rycardofarias/ordem_de_servico/service/ClienteService.java
@@ -1,7 +1,11 @@
 package com.github.rycardofarias.ordem_de_servico.service;
 
 import com.github.rycardofarias.ordem_de_servico.domain.Cliente;
+import com.github.rycardofarias.ordem_de_servico.domain.Pessoa;
+import com.github.rycardofarias.ordem_de_servico.dto.ClienteDTO;
 import com.github.rycardofarias.ordem_de_servico.repository.ClienteRepository;
+import com.github.rycardofarias.ordem_de_servico.repository.PessoaRepository;
+import com.github.rycardofarias.ordem_de_servico.service.exceptions.DataIntegratyViolationException;
 import com.github.rycardofarias.ordem_de_servico.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +18,11 @@ public class ClienteService {
 
     @Autowired
     private ClienteRepository clienteRepository;
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+
     public Cliente findById(Integer id) {
         Optional<Cliente> obj = clienteRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -23,5 +32,36 @@ public class ClienteService {
 
     public List<Cliente> findAll() {
         return clienteRepository.findAll();
+    }
+
+    private Pessoa findByCPF(ClienteDTO objDTO){
+        Pessoa obj = pessoaRepository.findByCPF(objDTO.getCpf());
+        if(obj != null){
+            return obj;
+        }
+        return null;
+    }
+
+    public Cliente create(ClienteDTO clienteDTO) {
+        if(findByCPF(clienteDTO) != null){
+            throw new DataIntegratyViolationException("CPF já cadastrado na base de dados.");
+        }
+        return clienteRepository.save(
+                new Cliente( null, clienteDTO.getNome(), clienteDTO.getCpf(), clienteDTO.getTelefone())
+        );
+    }
+
+    public Cliente update(Integer id, ClienteDTO clienteDTO) {
+        Cliente oldObj = findById(id);
+
+        if (findByCPF(clienteDTO) != null && findByCPF(clienteDTO).getId() != id){
+            throw new DataIntegratyViolationException("CPF já cadastrado na base de dados.");
+        }
+
+        oldObj.setNome(clienteDTO.getNome());
+        oldObj.setCpf(clienteDTO.getCpf());
+        oldObj.setTelefone(clienteDTO.getTelefone());
+
+        return clienteRepository.save(oldObj);
     }
 }
